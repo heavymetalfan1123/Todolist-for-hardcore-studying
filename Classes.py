4a43b5a9e18736a6cf242bca78ec37cc39838703
chronical depression/alcoholism <72998757+heavymetalfan1123@users.noreply.github.com>
2025-09-23 20:25:00 +0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,6 +1,68 @@
 import time
 import csv
 import os 
+import sqlite3
+
+
+
+
+
+
+
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#
+#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def anigilate(date1,date2):
@@ -11,84 +73,109 @@ def anigilate(date1,date2):
 class Todolist:
     def __init__(self,filename):
         self.filename = filename
-        self.NST_arr = []
+       
         self.respect = 0 
-        
-        if f"{self.filename}.csv" not in os.listdir():
-            with open(f'{self.filename}.csv',"w", newline='',encoding='utf-8') as csvfile:
-                pass
-        else:
-            with open(f'{self.filename}.csv',"r+", newline='',encoding='utf-8') as csvfile:
-                
-                spamreader = csv.reader(csvfile)
-                for row in spamreader:
-                    self.NST_arr.append(row)
-    
-    def create(self,name,text,time):
+        self.connect = sqlite3.connect(f"{self.filename}.db")
 
-        self.NST_arr.append([name,text,time,False])
+        self.cursr = self.connect.cursor()
+        self.cursr.execute("""CREATE TABLE IF NOT EXISTS users 
+                    (id INTEGER PRIMARY KEY,
+                    name TEXT
+                    
+                    )
+                    
+                    """)
+
+        self.cursr.execute("""CREATE TABLE IF NOT EXISTS tasks 
+                    (id INTEGER PRIMARY KEY,
+                        userid INTEGER,
+                        name TEXT,
+                        text TEXT,
+                        time INT,
+                        status BOOL,
+                        FOREIGN KEY (userid) REFERENCES users (id)
+                    )""")
+        # Эта строчка кода заменяется на cursore.execute
+        
+        self.connect.commit()
+    def create(self,name,text,time):
+        # Эта строчка заменяется на insert 
+        self.cursr.execute(f"""INSERT INTO tasks (id,userid,name,text,time,Status) VALUES(1,11,'выжить','Алкоголизм',1231241232,false)  """)
+        #self.cursr.fetchall
         self.sync()
 
     def Read(self):
-        return self.NST_arr
+        # эта строчка заменяется на select 
+        self.cursr.execute("""SELECT * FROM tasks
+                    """)
+        return self.cursr.fetchall()
 
     def update_task(self,num_of_change,status):
+        # ВОТ ЭТО Я ХЕЗЕ КАК ЗАМЕНИТЬ 
         try:
-            if  self.NST_arr != [] and int(num_of_change)<=len(self.NST_arr):
-                
-                    
-                    num_of_change = int(num_of_change)
-                    num_of_change-=1
-                    #print(*self.NST_arr[:num_of_change],*self.NST_arr[num_of_change:])
-                    
-                    if status.lower() == "true":
-                    
-                        self.NST_arr[num_of_change][3] = True
-                        print(anigilate(time.time(),self.NST_arr[num_of_change][2]))
-                        if anigilate(time.time(),self.NST_arr[num_of_change][2]) >0 :
+            
 
-                            self.respect+=1
-                        else:
-                            self.respect-=1
-                        self.NST_arr.pop(num_of_change)
-                    else:
-                        print("TASK NOT MADE")
+                    
+            num_of_change = int(num_of_change)
+            self.cursr.execute(f"""UPDATE tasks SET status={status} WHERE id = {num_of_change}
+                    """)
+                    
+                    
+                    
+            # if status.lower() == "true" or status.lower() == "1":
+                    
+                    
+            #     print(anigilate(time.time(),self.NST_arr[num_of_change][2]))
+            #     if anigilate(time.time(),self.NST_arr[num_of_change][2]) >0 :
+
+            #         self.respect+=1
+            #     else:
+            #         self.respect-=1
                     
                 
-            else:
-                print("ERORRRRRR REMAAAAKEEEE")
+                    
+                
+            # else:
+            #     print("ERORRRRRR REMAAAAKEEEE")
         except:
             print("поаккуратней")
 
-            self.sync()
+        self.sync()
 
     def delete(self,num_of_change):
         
-        if  self.NST_arr != [] and int(num_of_change)<=len(self.NST_arr):
-            try:
+        try:
+            
+
                     
-                num_of_change = int(num_of_change)
-                num_of_change-=1
-                print(*self.NST_arr[:num_of_change],*self.NST_arr[num_of_change+1:])
+            num_of_change = int(num_of_change)
+            self.cursr.execute(f"""DELETE FROM tasks WHERE id={num_of_change}
+                    """)
                     
-                self.NST_arr = self.NST_arr[:num_of_change]+self.NST_arr[num_of_change+1:]
                     
-            except:
-                print("ХЕЗЕ НУ ОШИБКА")
-        else:
-            print("ERROR")
+                    
+                # if status.lower() == "true":
+                    
+                #     self.NST_arr[num_of_change][3] = True
+                #     print(anigilate(time.time(),self.NST_arr[num_of_change][2]))
+                #     if anigilate(time.time(),self.NST_arr[num_of_change][2]) >0 :
+
+                #         self.respect+=1
+                #     else:
+                #         self.respect-=1
+                    
+                # else:
+                #     print("TASK NOT MADE")
+                    
+                
+            # else:
+            #     print("ERORRRRRR REMAAAAKEEEE")
+        except:
+            print("поаккуратней")
+
+        self.sync()
         
         self.sync()
     def sync(self):
-        # i = 0
-        # for el in self.NST_arr:
-        #     if True in el:
-        #         self.NST_arr.delete(i)
-        #     i+=1  
-
-        with open(f"{self.filename}.csv","w", newline="",encoding='utf-8') as newfile:
-            writ = csv.writer(newfile)
-            for el in self.NST_arr:
-                writ.writerow(el)
-    
+        self.connect.commit()
     
