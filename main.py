738575a2ac9119dc41653ef34ab72cdaf0962ad2
chronical depression/alcoholism <72998757+heavymetalfan1123@users.noreply.github.com>
2025-09-23 20:56:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,191 @@
-from Classes import Todolist
+
 import os
 import random
+import time
+import csv
+import os 
+import sqlite3
+
+
+
+
+
+
+
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#
+#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#       ,-=-.       ______     _
+#      /  +  \     />----->  _| |_
+#      | ~~~ |    // -/- /  |_   _|
+#      |R.I.P|   //  /  /     | |
+# \vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def anigilate(date1,date2):
+        
+        y2 = time.strptime(date2,"%H.%M.%d.%m.%Y")
+        
+        return time.mktime(y2)-date1   
+class Todolist: 
+    def __init__(self,filename):
+        self.filename = filename
+       
+        self.respect = 0 
+        self.connect = sqlite3.connect(f"{self.filename}.db")
+
+        self.cursr = self.connect.cursor()
+        self.cursr.execute("""CREATE TABLE IF NOT EXISTS users 
+                    (id INTEGER PRIMARY KEY,
+                    name TEXT
+                    
+                    )
+                    
+                    """)
+
+        self.cursr.execute("""CREATE TABLE IF NOT EXISTS tasks 
+                    (id INTEGER PRIMARY KEY,
+                        userid INTEGER,
+                        name TEXT,
+                        text TEXT,
+                        time TEXT,
+                        status BOOL,
+                        FOREIGN KEY (userid) REFERENCES users (id)
+                    )""")
+        # Эта строчка кода заменяется на cursore.execute
+        
+        self.connect.commit()
+    def create(self,name,text,time):
+        # Эта строчка заменяется на insert 
+        self.cursr.execute("""SELECT * FROM tasks""")
+        result = self.cursr.fetchall()
+        print(len(result))
+        self.cursr.execute(f"""INSERT INTO tasks (id,userid,name,text,time,Status) VALUES({len(result)+1},1,'{name}','{text}','{time}',false)  """)
+        #self.cursr.fetchall
+        self.sync()
+
+    def Read(self):
+        # эта строчка заменяется на select 
+        self.cursr.execute("""SELECT * FROM tasks
+                    """)
+        return self.cursr.fetchall()
+
+    def update_task(self,num_of_change,status):
+        # ВОТ ЭТО Я ХЕЗЕ КАК ЗАМЕНИТЬ 
+        try:
+            
+
+                    
+            num_of_change = int(num_of_change)
+            self.cursr.execute(f"""UPDATE tasks SET status={status} WHERE id = {num_of_change}
+                    """)
+                    
+                    
+                    
+            # if status.lower() == "true" or status.lower() == "1":
+                    
+                    
+            #     print(anigilate(time.time(),self.NST_arr[num_of_change][2]))
+            #     if anigilate(time.time(),self.NST_arr[num_of_change][2]) >0 :
+
+            #         self.respect+=1
+            #     else:
+            #         self.respect-=1
+                    
+                
+                    
+                
+            # else:
+            #     print("ERORRRRRR REMAAAAKEEEE")
+        except:
+            print("поаккуратней")
+
+        self.sync()
+
+    def delete(self,num_of_change):
+        
+        try:
+            
+
+                    
+            num_of_change = int(num_of_change)
+            self.cursr.execute(f"""DELETE FROM tasks WHERE id={num_of_change}
+                    """)
+                    
+                    
+                    
+                # if status.lower() == "true":
+                    
+                #     self.NST_arr[num_of_change][3] = True
+                #     print(anigilate(time.time(),self.NST_arr[num_of_change][2]))
+                #     if anigilate(time.time(),self.NST_arr[num_of_change][2]) >0 :
+
+                #         self.respect+=1
+                #     else:
+                #         self.respect-=1
+                    
+                # else:
+                #     print("TASK NOT MADE")
+                    
+                
+            # else:
+            #     print("ERORRRRRR REMAAAAKEEEE")
+        except:
+            print("поаккуратней")
+
+        self.sync()
+        
+        self.sync()
+    def sync(self):
+        self.connect.commit()
+    
+
 dec = Todolist("List")
 
 
@@ -50,7 +235,7 @@ dec = Todolist("List")
     #print(words_arr)
 #print("|ДОБРО ПОЖАЛОВАТЬ В АНКИ ДЛЯ БЕДНЫХ|\n|КРАТКАЯ НАВИГАЦИЯ ПО КОМАНДАМ|\n|(1)запись - запись значений (1)   |\n|(2)запоминание - режим запоминания(2)|\n|(3)вывод - вывод словаря в формате Слово-значение-контекст(3)  |\n|выход - выход из програмы     |")
 continuing = True
-#os.system('cls' if os.name == 'nt' else 'clear')
+os.system('cls' if os.name == 'nt' else 'clear')
 
 while continuing:
     #os.system('cls' if os.name == 'nt' else 'clear')
@@ -79,8 +264,8 @@ while continuing:
             for _ in range(n):
                 word = input("Слово:")
                 meaning = input("Значение:")
-                time = input("Время в формате %H.%M.%d.%m.%Y")
-                dec.create(word,meaning,time)
+                times = input("Время в формате %H.%M.%d.%m.%Y")
+                dec.create(word,meaning,times)
         except:
             print("Неправильный ввод начинайте по новой")
     
